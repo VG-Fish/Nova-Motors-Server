69f9fe2e12b72084819a37adedc04fa027eb73b5
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-06-27 01:09:18 -0500

    returns a dict

--- server.py
+++ server.py
@@ -1,19 +1,18 @@
 from flask import Flask
 import google.generativeai as genai, os
 from dotenv import load_dotenv
-from portpicker import pick_unused_port
 
 # Create Flask App
 app = Flask(__name__)
 
-# Get Gemini Key
+# Get gemini key
 load_dotenv(".env")
 genai.configure(api_key=os.getenv("KEY"))
 # The model
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 # To get gemini responses
-@app.route("/")
-def get_gemini_response(prompt: str = "Say 'Generating...'") -> str:
-    response = model.generate_content(prompt)
-    return response.text
+@app.route("/gemini")
+def get_gemini_response(prompt: str = "Say 'Generating...'") -> dict:
+    response =  model.generate_content(prompt)
+    return response.to_dict()
