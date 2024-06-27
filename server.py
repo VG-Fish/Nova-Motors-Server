363d7fc1e9ab04f60754a51526e7fc8eb6de5b6c
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-06-27 12:33:35 -0500

    updated code

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import google.generativeai as genai, os
 from dotenv import load_dotenv
 
@@ -12,7 +12,9 @@ genai.configure(api_key=os.getenv("KEY"))
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 # To get gemini responses
-@app.route("/gemini")
+@app.route("/gemini", methods=['GET', 'POST'])
 def get_gemini_response(prompt: str = "Say 'Generating...'") -> dict:
-    response =  model.generate_content(prompt)
+    prompt = request.args.get("prompt")
+    prompt = prompt if prompt else "Say 'Generating...'"
+    response = model.generate_content(prompt)
     return response.to_dict()
