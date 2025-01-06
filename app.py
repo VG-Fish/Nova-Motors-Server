1c7cf59be417106947965f0a1cf3b98910194a8d
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-01-05 19:05:37 -0600

    updated app for vercel

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ from dotenv import load_dotenv
 
 # Create Flask App
 app = Flask(__name__)
+# App is in Production Mode
+app.debug = False
 
 # Get gemini key
 load_dotenv(".env")
 genai.configure(api_key=os.getenv("KEY"))
 # The model
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel("gemini-1.5-flash")
+
 
 # To get gemini responses
-@app.route("/gemini", methods=['GET', 'POST'])
+@app.route("/gemini", methods=["GET", "POST"])
 def get_gemini_response(prompt: str = "Say 'Generating...'") -> dict:
     prompt = request.args.get("prompt")
     prompt = prompt if prompt else "Say 'Generating...'"
